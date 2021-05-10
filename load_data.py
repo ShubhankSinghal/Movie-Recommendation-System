fcc8ef9a9e4a957c7b87579f40c5d1fb6dbeece4
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-05-11 00:55:55 +0530

    complete util function

--- load_data.py
+++ load_data.py
@@ -82,3 +82,99 @@ class Data:
                     break
 
         return userRatings
+
+    def getPopularityRanks(self):
+        """
+            This function rank the movies based on their count of ratings
+        """
+
+        # initialised two dictionaries for our task
+        ratings = defaultdict(int)
+        rankings = defaultdict(int)
+
+        # calculated total count of rating a movie get
+        with open(self.ratingsPath, newline='') as csvfile:
+            ratingReader = csv.reader(csvfile)
+            next(ratingReader)
+            for row in ratingReader:
+                movieID = int(row[1])
+                ratings[movieID] += 1
+
+        # providing rank to the movies, based on count
+        rank = 1
+        for movieID, ratingCount in sorted(ratings.items(), key=lambda x: x[1], reverse=True):
+            rankings[movieID] = rank
+            rank += 1
+        return rankings
+
+    def getGenres(self):
+        """
+            This function returns the genre sparse list
+        """
+        genres = defaultdict(list)
+        genreIDs = {}
+        maxGenreID = 0
+
+        # Here, fetch the positions for the genre for each movie
+        with open(self.moviesPath, newline='', encoding='ISO-8859-1') as csvfile:
+            movieReader = csv.reader(csvfile)
+            next(movieReader)  # Skip header line
+            for row in movieReader:
+                movieID = int(row[0])
+                genreList = row[2].split('|')
+                genreIDList = []
+                for genre in genreList:
+                    if genre in genreIDs:
+                        genreID = genreIDs[genre]
+                    else:
+                        genreID = maxGenreID
+                        genreIDs[genre] = genreID
+                        maxGenreID += 1
+                    genreIDList.append(genreID)
+                genres[movieID] = genreIDList
+
+        # Convert integer-encoded genre lists to bitfields that we can treat as vectors
+        for (movieID, genreIDList) in genres.items():
+            bitfield = [0] * maxGenreID
+            for genreID in genreIDList:
+                bitfield[genreID] = 1
+            genres[movieID] = bitfield
+
+        return genres
+
+    def getYears(self):
+        """
+            This function returns the dictionary {movieId:Year}
+        """
+        # regular expression to extract year
+        p = re.compile(r"(?:\((\d{4})\))?\s*$")
+        years = defaultdict(int)
+        with open(self.moviesPath, newline='', encoding='ISO-8859-1') as csvfile:
+            movieReader = csv.reader(csvfile)
+            next(movieReader)
+            for row in movieReader:
+                movieID = int(row[0])
+                title = row[1]
+                m = p.search(title)
+                year = m.group(1)
+                if year:
+                    years[movieID] = int(year)
+        return years
+
+    def getMovieName(self, movieID):
+        """
+            This function will return the movie name from movieID
+        """
+        if movieID in self.movieID_to_name:
+            return self.movieID_to_name[movieID]
+        else:
+            return ""
+
+    def getMovieID(self, movieName):
+        """
+            This function will return the movieId from movieName
+        """
+        if movieName in self.name_to_movieID:
+            return self.name_to_movieID[movieName]
+        else:
+            return 0
